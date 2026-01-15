bb447bd9c3395a36a6aa7855743c0a532f014bf2
[dyad] <gabrielschimtez@gmail.com>
2026-01-15 20:45:11 -0300

    [dyad] Corrigido erro de importação do ícone Motorbike para Motorcycle - wrote 1 file(s)

--- src/components/ui/MotorcycleIcon.tsx
+++ src/components/ui/MotorcycleIcon.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { cn } from "@/lib/utils";
-import { Motorbike } from "lucide-react";
+import { Motorcycle } from "lucide-react";
 
 interface MotorcycleIconProps extends React.SVGProps<SVGSVGElement> {
   className?: string;
 }
 
 export function MotorcycleIcon({ className, ...props }: MotorcycleIconProps) {
-  // Usando o ícone Motorbike do Lucide React
+  // Usando o ícone Motorcycle do Lucide React
   return (
-    <Motorbike className={cn("w-6 h-6", className)} {...props} />
+    <Motorcycle className={cn("w-6 h-6", className)} {...props} />
   );
 }
