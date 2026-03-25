98416119f789d748fd1b3698e954d970a5570bd8
[dyad] <gabrielschimtez@gmail.com>
2026-03-25 11:32:17 -0300

    [dyad] Installing missing vite-plugin-pwa dependency. - wrote 3 file(s), added vite-plugin-pwa package(s) + extra files edited outside of Dyad

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,4 +1,8 @@
-import * as ResizablePrimitive from "react-resizable-panels";
+import {
+  Panel,
+  Group,
+  Separator,
+} from "react-resizable-panels";
 import { GripVertical } from "lucide-react";
 
 import { cn } from "@/lib/utils";
@@ -6,8 +10,8 @@ import { cn } from "@/lib/utils";
 const ResizablePanelGroup = ({
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
-  <ResizablePrimitive.PanelGroup
+}: React.ComponentProps<typeof Group>) => (
+  <Group
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
       className
@@ -16,16 +20,16 @@ const ResizablePanelGroup = ({
   />
 );
 
-const ResizablePanel = ResizablePrimitive.Panel;
+const ResizablePanel = Panel;
 
 const ResizableHandle = ({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
+}: React.ComponentProps<typeof Separator> & {
   withHandle?: boolean;
 }) => (
-  <ResizablePrimitive.PanelResizeHandle
+  <Separator
     className={cn(
       "relative flex w-px items-center justify-center bg-border transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-4 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
@@ -37,7 +41,7 @@ const ResizableHandle = ({
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
-  </ResizablePrimitive.PanelResizeHandle>
+  </Separator>
 );
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle };
